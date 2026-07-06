1daf9669ad86838a4ccaca0b96886c89d1716410
MD Mazed Hossain <147969937+MH-Parag@users.noreply.github.com>
2026-07-06 20:38:04 +0600

    Update cses_1083.cpp
    This is approach 1

--- Jun24/cses_1083.cpp
+++ Jun24/cses_1083.cpp
@@ -33,29 +33,3 @@ int main() {
  
 	return 0;
 }
-
-
-
-
-
-
-// approach 2
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long int;
- 
-int main() {
-    ll n;
-    cin>>n;
-    ll arr[n-1];
-    for(ll i=0; i<n-1; i++){
-        cin>>arr[i];
-    }
-    ll expected_sum = (n*(n+1))/2;
-    ll current_sum = 0;
-    for(ll i=0; i<n-1; i++){
-        current_sum += arr[i];
-    }
-    cout<<expected_sum - current_sum <<"\n";
-	return 0;
-}
